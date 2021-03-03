b94f4027c17cb32728a9e082c368dc1209eab01f
jonas.gomes <jonas.gomes@bavabank.com>
2021-03-03 15:18:27 -0300

    ♻️ Renamed method;

--- src/client.go
+++ src/client.go
@@ -21,7 +21,7 @@ var once sync.Once
 	@param dsn Data source name
 	example: dsn := "host=localhost user=gorm password=gorm dbname=gorm port=9920 sslmode=disable TimeZone=Asia/Shanghai"
 */
-func PostgreSQLConnect(dsn string) *dbclient {
+func Connect(dsn string) *dbclient {
 	once.Do(func() {
 		db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
 			NamingStrategy: schema.NamingStrategy{
